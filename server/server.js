f3ba376f886960f56a7b36706f4fa313daa78c47
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-03-17 18:57:28 -0600

    using middleware

--- server/server.js
+++ server/server.js
@@ -31,10 +31,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/js", (req, res) => {
-    rollbar.info("Served JS");
-    res.sendFile(path.join(__dirname, "../public"));
-});
+app.use(express.static(path.join(__dirname, "../public")))
+
+// app.get("/js", (req, res) => {
+//     rollbar.info("Served JS");
+//     res.sendFile(path.join(__dirname, "../public"));
+// });
 
 app.post("/yo", (req, res) => {
     rollbar.info(`We have received ${req.body.text}`);
