989e6d42169e7b683110e1942502e131fef07f38
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-03-17 18:11:42 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'b2a75d0453c04c348e8f8af2bd861e9f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+// Serving files using middleware (express.static)
+app.use("/", function(){
+    express.static(path.join(__dirname, "../public"))
+    rollbar.log("Hit the endpoint")
+});
+
+// Serving files using endpoints (app.get())
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
