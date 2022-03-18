684fc37ceec081df5492dc9a6c579dba4cf135fe
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-03-17 18:47:57 -0600

    jeddy youre an idiot ad dyour files

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/main.js"));
+});
+
 app.post("/yo", (req, res) => {
     rollbar.info(`We have received ${req.body.text}`);
     res.sendStatus(200);
